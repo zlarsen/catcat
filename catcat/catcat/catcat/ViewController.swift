5b40fd3c1d0ee1efc128bc5416e89e0084518d5b
zlarsen <zlarsen@dmail.dixie.edu>
2016-05-20 20:14:00 -0600

    adds shake functionality

--- catcat/catcat/catcat/ViewController.swift
+++ catcat/catcat/catcat/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var shakeLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +20,16 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func canBecomeFirstResponder() -> Bool {
+        return true
+    }
+    
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
+        if motion == .MotionShake {
+            self.shakeLabel.text = "Shaken, not stirred"
+        }
+    }
 
 }
 
