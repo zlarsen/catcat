8e4be0f9a190d412c2ac7faa3a43846e2b4e3576
zlarsen <zlarsen@dmail.dixie.edu>
2016-05-24 16:58:38 -0600

    adds beginning of achievements table

--- catcat/catcat/catcat/AchievementsViewController.swift
+++ catcat/catcat/catcat/AchievementsViewController.swift
@@ -8,10 +8,12 @@
 
 import UIKit
 
-class AchievementsViewController: UIViewController {
+class AchievementsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-    var achievements: [String: String] = ["shake 1":"Shaken Not Stirred"]
-    var newAchievements: String = ""
+    var achievementNames: [String] = ["Shaken Not Stirred", "Don't shake me"]
+    var achievementValues: [String] = ["shake 1", "shake 10"]
+    
+    @IBOutlet var achievementsTable: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +26,10 @@ class AchievementsViewController: UIViewController {
         let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(AchievementsViewController.respondToSwipeGesture(_:)))
         swipeRight.direction = UISwipeGestureRecognizerDirection.Right
         self.view.addGestureRecognizer(swipeRight)
+        
+        self.achievementsTable.tableFooterView = UIView()
+        
+//        setupAchievementsTable()
         
     }
     
@@ -53,5 +59,35 @@ class AchievementsViewController: UIViewController {
         let resultViewController = storyBoard.instantiateViewControllerWithIdentifier("cat")
         resultViewController.modalTransitionStyle = UIModalTransitionStyle.CrossDissolve
         self.presentViewController(resultViewController, animated:true, completion:nil)
+    }
+    
+    func setupAchievementsTable() {
+        achievementsTable.beginUpdates()
+        print("*******************2")
+        achievementsTable.insertRowsAtIndexPaths([
+            NSIndexPath(forRow: achievementNames.count-1, inSection: 0)
+            ], withRowAnimation: .Automatic)
+        print("*******************3")
+        achievementsTable.endUpdates()
+        print("hello")
+    }
+    
+    func numberOfSectionsInTableView(achievementsTable: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(achievementsTable: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return achievementNames.count
+    }
+    
+    func tableView(achievementsTable: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = achievementsTable.dequeueReusableCellWithIdentifier("cell",
+        forIndexPath: indexPath)
+        let item = achievementNames[indexPath.row]
+        cell.textLabel?.text = item
+        cell.textLabel?.textAlignment = NSTextAlignment.Center
+        cell.textLabel?.textColor = UIColor.whiteColor()
+        print("row: \(indexPath.row), title: \(cell.textLabel!.text!)")
+        return cell
     }
 }
